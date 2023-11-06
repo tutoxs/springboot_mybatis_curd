b3ba1af57ffc43f0eaef769bdb564d5e30cd0ef9
陈啸闯 <cxc0705@gmail.com>
2023-11-06 18:07:13 +0800

    增删改查

--- src/main/java/com/example/mapper/EmpMapper.java
+++ src/main/java/com/example/mapper/EmpMapper.java
@@ -1,16 +1,46 @@
 package com.example.mapper;
 
 import com.example.pojo.Emp;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Mapper
 public interface EmpMapper {
+//    删除员工
     @Delete("delete from tb_emp where id=#{id}")
     public int delete(Integer id);
-
+//添加员工
+    @Options(useGeneratedKeys = true,keyProperty = "id")
     @Insert("insert into tb_emp(username, name, gender, image, job, entrydate, dept_id, create_time, update_time)" +
             "values (#{username},#{name},#{gender},#{image},#{job},#{entryDate},#{deptId},#{createTime},#{updateTime});")
     public void insert(Emp emp);
+
+//    更新员工
+    @Update("update tb_emp set username = #{username},name=#{name},gender=#{gender},image=#{image},job=#{job},entrydate=#{entryDate},dept_id=#{deptId},update_time=#{updateTime} where id= #{id}")
+    public void update(Emp emp);
+
+//    单条查询
+//    开启驼峰映射
+    @Select("select * from tb_emp where id=#{id}")
+    public Emp getById(Integer id);
+
+//    起别名，保持属性名一致
+//@Select("select id, username, password, name, gender, image, job, entrydate, dept_id deptId, create_time createTime, update_time updateTime from tb_emp where id=#{id}")
+//public Emp getById(Integer id);
+
+//    Results手动映射封装
+//    @Results({
+//            @Result(column = "dept_id",property = "deptId"),
+//            @Result(column = "create_time",property = "createTime"),
+//            @Result(column = "update_time",property = "updateTime"),
+//    })
+//    @Select("select * from tb_emp where id=#{id}")
+//    public Emp getById(Integer id);
+
+//    条件查询
+    @Select("select * from tb_emp where name like concat('%',#{name},'%') and gender=#{gender} and entrydate between #{start} and #{end};")
+    public List<Emp> list(String name, short gender, LocalDate start,LocalDate end);
+
 }
--- src/test/java/com/example/SpringbootMybatisCurdApplicationTests.java
+++ src/test/java/com/example/SpringbootMybatisCurdApplicationTests.java
@@ -8,23 +8,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 class SpringbootMybatisCurdApplicationTests {
     @Autowired
     private EmpMapper empMapper;
-
+//删除
     @Test
     public void testDelete() {
         int deleteNum = empMapper.delete(16);
         System.out.println(deleteNum);
     }
-
+//添加
     @Test
     public void testInsert(){
         Emp emp=new Emp();
-        emp.setUsername("tom");
-        emp.setName("汤姆");
+        emp.setUsername("tom2");
+        emp.setName("汤姆2");
         emp.setImage("1.jpg");
         emp.setGender((short)1);
         emp.setJob((short)1);
@@ -34,6 +35,35 @@ class SpringbootMybatisCurdApplicationTests {
         emp.setDeptId(1);
 
         empMapper.insert(emp);
+        System.out.println(emp.getId());
+    }
+//更新
+    @Test
+    public void testUpdate(){
+        Emp emp=new Emp();
+        emp.setId(10);
+        emp.setUsername("tom10");
+        emp.setName("汤姆10");
+        emp.setImage("1.jpg");
+        emp.setGender((short)1);
+        emp.setJob((short)1);
+        emp.setEntryDate(LocalDate.of(2000,1,1));
+        emp.setUpdateTime(LocalDateTime.now());
+        emp.setDeptId(1);
+
+        empMapper.update(emp);
     }
 
+//    查询
+    @Test
+    public void testGetById(){
+        Emp emp=empMapper.getById(8);
+        System.out.println(emp);
+    }
+//   条件查询
+    @Test
+    public void testSelect(){
+        List<Emp> empList =empMapper.list("张",(short) 1,LocalDate.of(2010,10,10),LocalDate.of(2022,10,10));
+        System.out.println(empList);
+    }
 }
